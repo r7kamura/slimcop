b0b5a4fbeceaafc69f72d3c3e127e2cf6e800617
Ryo Nakamura <r7kamura@gmail.com>
2021-12-25 18:46:37 +0900

    Support glob pattern on arguments of executable

---
+++ spec/slimcop/file_collector_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+RSpec.describe Slimcop::FileCollector do
+  describe '#call' do
+    subject do
+      described_class.new(
+        patterns: patterns
+      ).call
+    end
+
+    let(:patterns) do
+      raise NotImplementedError
+    end
+
+    context 'with normal file path' do
+      let(:patterns) do
+        %w[spec/fixtures/dummy.slim]
+      end
+
+      it 'returns expected file paths' do
+        is_expected.to eq(patterns)
+      end
+    end
+
+    context 'with glob pattern' do
+      let(:patterns) do
+        %w[spec/**/*.slim]
+      end
+
+      it 'returns expected file paths' do
+        is_expected.to eq(%w[spec/fixtures/dummy.slim])
+      end
+    end
+
+    context 'with directory path' do
+      let(:patterns) do
+        %w[spec/fixtures]
+      end
+
+      it 'excludes directies' do
+        is_expected.to be_empty
+      end
+    end
+  end
+end
