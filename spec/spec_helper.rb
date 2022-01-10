6f5190c273f2363f1ef57ddada5b50753e72a0ac
Ryo Nakamura <r7kamura@gmail.com>
2022-01-10 10:42:14 +0900

    Allow to use :focus on RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,7 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
